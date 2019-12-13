3c8064e3c8b036b581c7358cd5488eb60c671df4
Michele Bevilacqua <michele.bev@outlook.com>
2019-12-13 22:50:19 +0100

    Fix, LED e calibrazione
    Aggiunto LED RGB che cambia colore in base alla distanza, commenti al codice e calibrato il suono, la sua durata e la distanza in cui avviare il cicalino.

--- sensori_parcheggio.ino
+++ sensori_parcheggio.ino
@@ -1,10 +1,12 @@
-//Scritto da Michele Bevilacqua , Univpm, Ancona.
-//Data dell'ultima revisione: 13/12/2019
+//Scritto da Michele Bevilacqua, Univpm, Ancona.
+//Data dell'ultima revisione: 13/12/2019 22:50
 
 #include "pitches.h" //include il file con macro al preprocessore con le frequenze di varie note
 #include "SR04.h"  //libreria per il calcolo della distanza
 #define TRIG_PIN  12//pin digitale
 #define ECHO_PIN  11//pin digitale
+#define RED 3
+#define GREEN 6
 
 long distanza;
 int intervallo;
@@ -13,34 +15,63 @@ unsigned long t0, currentMillis; //t0 memorizza il millisecondo in cui si è avv
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 
 void setup() {
+  
   t0 = millis();
+  
+  //inizializzo il led rgb con i vari pin
+  pinMode(RED,OUTPUT);
+  pinMode(GREEN,OUTPUT);
+  digitalWrite(RED,LOW);
+  digitalWrite(GREEN,LOW);
 }
 
 void loop() {
    distanza = sr04.Distance();
    
-   if(distanza <=5)
+   if(distanza <=15)
    {
-    tone(8, NOTE_E5, 500);//emette il suono dal cicalino passivo, la nota NOTE_E5 è definita in pitches.h
+    tone(8, NOTE_E6, 1000);//emette il suono dal cicalino passivo, la nota NOTE_E6 è definita in pitches.h
+    digitalWrite(RED,HIGH);
+    digitalWrite(GREEN, LOW);
+      if(distanza <=5)
+      {
+        tone(8, NOTE_B6, 1000);  
+      }
    }
-   else if(distanza >= 60)
+   else if(distanza >= 80)
    {
     noTone(8);//spegne eventuali suoni 
+    digitalWrite(RED,LOW);
+    digitalWrite(BLUE,LOW);
+    digitalWrite(GREEN,LOW);
    }
    else
    {
-    intervallo = map(distanza, 5,70,400,1300);//calcola un valore tra 400 e 1300 in proporzione al valore che assume nell'intervallo 5-70
-    emettiSuono(intervallo);
+      //calcola il valore corrispondente tra 400 e 1500 
+      //in proporzione al valore che assume nell'intervallo 15-80 cm
+      intervallo = map(distanza, 15,80,400,1500);
+  
+      if(distanza <=40)
+      {
+        digitalWrite(RED,HIGH);
+        digitalWrite(GREEN,HIGH);
+      }
+      else
+      {
+        digitalWrite(RED, LOW);
+        digitalWrite(GREEN, HIGH);
+      }
+      emettiSuono(intervallo);
    }
 }
 
 void emettiSuono(int intervallo)
 {
-  currentMillis = millis();//ottiene i millisecondi correnti
+  currentMillis = millis();//ottiene i millisecondi attuali
   
   if(currentMillis - t0 >= intervallo)
   {
-    t0 = currentMillis;//ottiene i millisecondi correnti in cui si attiva il cicalino per ricalcolare l'intervallo
-    tone(8, NOTE_E5, 500);  
+    t0 = currentMillis;//ottiene i millisecondi attuali in cui si attiva il cicalino per ricalcolare l'intervallo
+    tone(8, NOTE_E7, 200);  
   }
 } 
